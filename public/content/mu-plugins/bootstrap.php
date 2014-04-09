cfb9a76003e65708ce3d55f0a6d94dbdef653f06
Christian de Botton <debotton@brooklynunited.com>
2014-04-09 13:53:23 -0400

    Custom post types.

--- public/content/mu-plugins/bootstrap.php
+++ public/content/mu-plugins/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use WPPlugins\WPExtend\ContentType;
+
 /**
  * Plugin Name: App Bootstrap
  * Depends: Classes
@@ -7,4 +9,13 @@
 
 add_action('plugins_loaded', function () {
 
+  $articles = new ContentType('articles', [
+    'singular_name' => 'Article',
+    'supports' => ['editor', 'title', 'revisions', 'custom-fields', 'thumbnail']
+  ]);
+
+  $projects = new ContentType('projects', [
+    'singular_name' => 'Projects',
+    'supports' => ['editor', 'title', 'revisions', 'custom-fields', 'thumbnail']
+  ]);
 });
