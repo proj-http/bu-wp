6fffa09c53b6f27ba0fc6e793fd57715fb74e299
Christian de Botton <debotton@brooklynunited.com>
2014-04-09 10:50:25 -0400

    Fixing brunch config.

--- brunch-config.js
+++ brunch-config.js
@@ -1,12 +1,12 @@
 exports.config = {
   paths: {
     watched: ['brunch', 'envs', 'bower_components', 'test', 'app', 'public'],
-    public: 'public/content/build'
+    public: 'public/build'
   },
   files: {
     javascripts: {
       joinTo: {
-        'javascripts/app.js': /^brunch/
+        'javascripts/app.js': /^brunch/,
         'javascripts/vendor.js': /^(bower_components|vendor)/
       }
     },
